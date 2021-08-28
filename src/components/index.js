9246ffa98cf40ef69f13de495c59f1b876728e52
ann-rotem <ann.rotem@gmail.com>
2021-08-28 22:04:38 +0300

    refactor: export components as default

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from "./Button/Button";
+export { default as Header } from "./Header/Header";
+//export { default as Icon } from "./Icon/Icon";
+export { default as Main } from "./Main/Main";
+export { default as Search } from "./Search/Search";
+export { default as ThemeToggler } from "./ThemeToggler/ThemeToggler";
