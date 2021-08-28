8108f95acd6acec6ce09d6b3eb84a28c86c11663
ann-rotem <ann.rotem@gmail.com>
2021-08-28 22:05:14 +0300

    style: add theme provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import GlobalStyle from "GlobalStyle";
+import { useState } from "react";
+import { GlobalStyle, lightTheme, darkTheme } from "GlobalStyle";
+import { ThemeProvider } from "styled-components";
+//import Search from "components/Search/Search";
+import { Header, Main } from "components";
+import * as S from "./styles";
 
 function App() {
+	const [theme, setTheme] = useState("dark");
+	const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+
 	return (
-		<>
+		<ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
 			<GlobalStyle />
-			<h1>Create React App Template</h1>
-		</>
+			<Header theme={theme} toggleTheme={toggleTheme} />
+			<Main />
+			<S.Footer>Footer </S.Footer>
+		</ThemeProvider>
 	);
 }
 
 export default App;
+
+// {
+// 	/* <S.Header>
+// 					<S.Logo screenSize={screenSize} width={width}>
+// 						<span>Weather</span> Forecast
+// 					</S.Logo>
+// 					<ThemeToggler theme={theme} handleChange={toggleTheme} />
+// 				</S.Header> */
+// }
