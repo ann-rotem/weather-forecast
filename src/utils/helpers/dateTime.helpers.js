643c9bddc4950fdfe488b9881a5b5444cfea83e3
ann-rotem <ann.rotem@gmail.com>
2021-08-28 22:02:29 +0300

    feat: add date and time helper functions

---
+++ src/utils/helpers/dateTime.helpers.js
@@ -0,0 +1,26 @@
+let date = new Date();
+
+export const getCurrentDate = () => {
+	let options = {
+		weekday: "short",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	};
+	return new Intl.DateTimeFormat("en-US", options).format(date);
+};
+
+export const getCurrentTime = () => {
+	let options = {
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: false,
+	};
+	return new Intl.DateTimeFormat("en-US", options).format(date);
+};
+
+export const isDay = () => {
+	return new Intl.DateTimeFormat("en-US", { dayPeriod: "narrow" }).format(
+		date
+	);
+};
