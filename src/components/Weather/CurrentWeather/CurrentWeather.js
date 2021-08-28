d4c3cf1b4efaca2432a3c8c4bc7bb976a23d3960
ann-rotem <ann.rotem@gmail.com>
2021-08-29 00:22:46 +0300

    feat: add current weather component

---
+++ src/components/Weather/CurrentWeather/CurrentWeather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./CurrentWeather.style";
+
+const CurrentWeather = () => {
+	return (
+		<>
+			<S.CurrentWeather>
+				<p>Current Weather component</p>
+			</S.CurrentWeather>
+		</>
+	);
+};
+
+export default CurrentWeather;
