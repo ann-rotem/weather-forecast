045ad1150c1c9ac49c17f14928a400b6221bed6e
ann-rotem <ann.rotem@gmail.com>
2021-08-29 00:22:28 +0300

    feat: add forecast component

---
+++ src/components/Weather/Forecast/Forecast.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Forecast.style";
+
+const Forecast = () => {
+	return (
+		<S.Forecast>
+			<p>Forecast component</p>
+		</S.Forecast>
+	);
+};
+
+export default Forecast;
