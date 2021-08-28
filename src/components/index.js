506a073ec41fd66006ad2b44146d5bcdac9e5aeb
ann-rotem <ann.rotem@gmail.com>
2021-08-29 00:23:32 +0300

    feat: add weather exports

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as Main } from "./Main/Main";
 export { default as Search } from "./Search/Search";
 export { default as Tabs } from "./Tabs/Tabs";
 export { default as ThemeToggler } from "./ThemeToggler/ThemeToggler";
+export * from "./Weather";
