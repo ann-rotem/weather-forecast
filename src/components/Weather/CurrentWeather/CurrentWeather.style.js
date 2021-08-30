8d2bbdb654234c47d580f84c65e626be386303d6
ann-rotem <ann.rotem@gmail.com>
2021-08-30 10:32:54 +0300

    style: add initial styles

--- src/components/Weather/CurrentWeather/CurrentWeather.style.js
+++ src/components/Weather/CurrentWeather/CurrentWeather.style.js
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const CurrentWeather = styled.div``;
+export const CurrentWeather = styled.div`
+	display: flex;
+	padding-top: 0.5rem;
+	color: ${({ theme }) => theme.textStrong};
+`;
+
+export const Column = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 50%;
+
+	p {
+		padding: 0.2rem 0;
+		font-size: 0.9rem;
+	}
+
+	h2 {
+		font-size: 3rem;
+		text-align: center;
+		font-weight: 800;
+	}
+`;
