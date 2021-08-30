99f23c37e302ee85ab8dd69e72af376adf412edd
ann-rotem <ann.rotem@gmail.com>
2021-08-30 10:32:36 +0300

    refactor: add data from api

--- src/components/Weather/CurrentWeather/CurrentWeather.js
+++ src/components/Weather/CurrentWeather/CurrentWeather.js
@@ -1,11 +1,25 @@
 import React from "react";
 import * as S from "./CurrentWeather.style";
+import { round, convertUnixTime } from "utils/helpers/dateTime.helpers";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
+	const sunrise = convertUnixTime(data.sunrise, "time");
+	const sunset = convertUnixTime(data.sunset, "time");
 	return (
 		<>
 			<S.CurrentWeather>
-				<p>Current Weather component</p>
+				<S.Column>
+					<h2>{round(data.temp)}°</h2>
+					<>{data.weather[0].icon}</>
+				</S.Column>
+				<S.Column>
+					<p>{convertUnixTime(data.dt, "date")}</p>
+					<p>Feels like: {round(data.feels_like)}°</p>
+					<p>{data.weather[0].description}</p>
+					<p>Humidity: {data.humidity}%</p>
+					<p>Sunrise: {sunrise}</p>
+					<p>Sunset: {sunset}</p>
+				</S.Column>
 			</S.CurrentWeather>
 		</>
 	);
