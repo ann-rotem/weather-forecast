d0369d205ed3b7c343dbaf995f5f0f231ca95b17
ann-rotem <ann.rotem@gmail.com>
2021-08-30 10:54:08 +0300

    refactor: move styled components to a new file

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,15 @@
 import React from "react";
-import styled from "styled-components";
+import * as S from "./Search.style";
 import { Button } from "components";
 import { SearchIcon } from "components/Icon";
 
 const Search = (props) => {
 	const { value, setValue, handleSubmit, disabled = false } = props;
 	return (
-		<Form onSubmit={handleSubmit}>
-			<Label htmlFor="search">Enter city, country, or zip code</Label>
-			<SearchBar>
-				<Input
+		<S.Form onSubmit={handleSubmit}>
+			<S.Label htmlFor="search">Enter city, country, or zip code</S.Label>
+			<S.SearchBar>
+				<S.Input
 					type="search"
 					id="search"
 					name="search"
@@ -22,53 +22,9 @@ const Search = (props) => {
 				/>
 
 				<Button type="icon" primary children={<SearchIcon size="lg" />} />
-			</SearchBar>
-		</Form>
+			</S.SearchBar>
+		</S.Form>
 	);
 };
 
 export default Search;
-
-const Form = styled.form`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	margin: 0 1rem;
-`;
-
-const Label = styled.label`
-	display: flex;
-	padding: 0.5rem;
-	color: ${({ theme }) => theme.textWeak};
-	font-weight: 300;
-`;
-
-const SearchBar = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	box-shadow: ${({ theme }) => theme.shadow};
-	background: ${({ theme }) => theme.backgroundSecondary};
-	border-radius: 1.5rem;
-`;
-
-const Input = styled.input`
-	display: inline-flex;
-	font-size: 1rem;
-	color: ${({ theme }) => theme.text};
-	background: transparent;
-	border: none;
-	border-radius: 1.5rem 0 0 1.5rem;
-	padding: 0.5rem;
-	width: 70%;
-	height: 100%;
-
-	:focus:not(:focus-visible) {
-		outline: none;
-	}
-
-	&:focus {
-		outline: 1px solid ${({ theme }) => theme.primaryColor};
-	}
-`;
---
+++ src/components/Search/Search.style.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin: 0 1rem;
+`;
+
+export const Label = styled.label`
+	display: flex;
+	padding: 0.5rem;
+	color: ${({ theme }) => theme.textWeak};
+	font-weight: 300;
+`;
+
+export const SearchBar = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	box-shadow: ${({ theme }) => theme.shadow};
+	background: ${({ theme }) => theme.backgroundSecondary};
+	border-radius: 1.5rem;
+`;
+
+export const Input = styled.input`
+	display: inline-flex;
+	font-size: 1rem;
+	color: ${({ theme }) => theme.text};
+	background: transparent;
+	border: none;
+	border-radius: 1.5rem 0 0 1.5rem;
+	padding: 0.5rem;
+	width: 70%;
+	height: 100%;
+
+	:focus:not(:focus-visible) {
+		outline: none;
+	}
+
+	&:focus {
+		outline: 1px solid ${({ theme }) => theme.primaryColor};
+	}
+`;
