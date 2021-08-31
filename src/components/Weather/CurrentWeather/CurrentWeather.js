f4bed0b1cb4698f27cf507416cbe09d4f7774f76
ann-rotem <ann.rotem@gmail.com>
2021-08-31 15:32:11 +0300

    refactor: add weather icons

--- src/components/Weather/CurrentWeather/CurrentWeather.js
+++ src/components/Weather/CurrentWeather/CurrentWeather.js
@@ -1,25 +1,25 @@
 import React from "react";
 import * as S from "./CurrentWeather.style";
+import { WeatherIcon } from "components/Icon";
 import { round, convertUnixTime } from "utils/helpers/dateTime.helpers";
 
 const CurrentWeather = ({ data }) => {
-	const sunrise = convertUnixTime(data.sunrise, "time");
-	const sunset = convertUnixTime(data.sunset, "time");
+	const sunrise = convertUnixTime(data.sys.sunrise, "time");
+	const sunset = convertUnixTime(data.sys.sunset, "time");
 	return (
 		<>
 			<S.CurrentWeather>
-				<S.Column>
-					<h2>{round(data.temp)}°</h2>
-					<>{data.weather[0].icon}</>
-				</S.Column>
-				<S.Column>
-					<p>{convertUnixTime(data.dt, "date")}</p>
-					<p>Feels like: {round(data.feels_like)}°</p>
+				<S.Temp>{round(data.main.temp)}°</S.Temp>
+				<S.Icon>
+					<WeatherIcon condition={data.weather[0].icon} size="120" />
+				</S.Icon>
+				<S.Details>
+					<p>Feels like {round(data.main.feels_like)}°</p>
 					<p>{data.weather[0].description}</p>
-					<p>Humidity: {data.humidity}%</p>
+					<p>Humidity: {data.main.humidity}%</p>
 					<p>Sunrise: {sunrise}</p>
 					<p>Sunset: {sunset}</p>
-				</S.Column>
+				</S.Details>
 			</S.CurrentWeather>
 		</>
 	);
