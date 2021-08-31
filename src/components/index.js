259faf58bda182d35eb2c46ac1307bae8677a2cc
ann-rotem <ann.rotem@gmail.com>
2021-08-31 15:30:37 +0300

    feat: add component for error message display

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button/Button";
+export { default as ErrorMessage } from "./ErrorMessage/ErrorMessage";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
 //export { default as Icon } from "./Icon/Icon";
