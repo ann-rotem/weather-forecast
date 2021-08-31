e8bb4153faf5aaec38ffb3c63935cbeba2096ae4
ann-rotem <ann.rotem@gmail.com>
2021-08-31 15:28:52 +0300

    refactor: add current location button

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./Search.style";
 import { Button } from "components";
-import { SearchIcon } from "components/Icon";
+import { SearchIcon, TargetIcon } from "components/Icon";
+//import { useGeolocation } from "utils/hooks";
 
-const Search = (props) => {
-	const { value, setValue, handleSubmit, disabled = false } = props;
+const Search = ({ handleSearch }) => {
+	const [query, setQuery] = useState("");
+	const handleChange = (e) => setQuery(e.target.value);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		handleSearch(query);
+		setQuery("");
+	};
+
 	return (
-		<S.Form onSubmit={handleSubmit}>
-			<S.Label htmlFor="search">Enter city, country, or zip code</S.Label>
-			<S.SearchBar>
-				<S.Input
-					type="search"
-					id="search"
-					name="search"
-					required
-					value={value}
-					disabled={disabled}
-					onChange={setValue}
-					minLength="2"
-					autoComplete="off"
-				/>
+		<S.SearchSection>
+			<Button onClick={handleSubmit} type="icon" secondary>
+				<TargetIcon size="lg" />
+			</Button>
+			<S.Form onSubmit={handleSubmit}>
+				<S.Label htmlFor="search">Enter city, country, or zip code</S.Label>
+				<S.SearchBar>
+					<S.Input
+						type="search"
+						id="search"
+						name="search"
+						required
+						value={query}
+						// disabled={disabled}
+						onChange={handleChange}
+						minLength="2"
+						autoComplete="off"
+					/>
 
-				<Button type="icon" primary children={<SearchIcon size="lg" />} />
-			</S.SearchBar>
-		</S.Form>
+					<Button
+						type="icon"
+						primary
+						children={<SearchIcon size="lg" />}
+					/>
+				</S.SearchBar>
+			</S.Form>
+		</S.SearchSection>
 	);
 };
 
