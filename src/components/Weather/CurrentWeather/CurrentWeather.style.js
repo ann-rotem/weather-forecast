759734267e10ae5ec5839b4316f13dc89cdd4a65
ann-rotem <ann.rotem@gmail.com>
2021-08-31 15:32:56 +0300

    style: change display from flex to grid

--- src/components/Weather/CurrentWeather/CurrentWeather.style.js
+++ src/components/Weather/CurrentWeather/CurrentWeather.style.js
@@ -1,24 +1,49 @@
 import styled from "styled-components";
 
 export const CurrentWeather = styled.div`
-	display: flex;
-	padding-top: 0.5rem;
-	color: ${({ theme }) => theme.textStrong};
+	color: ${({ theme }) => theme.text};
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: 1fr 1fr 2fr;
+	max-height: 18rem;
+	grid-template-areas:
+		"temp       temp"
+		"icon    details"
+		"icon details";
+	gap: 0.5rem;
+	padding: 0.5rem;
 `;
 
-export const Column = styled.div`
+export const Temp = styled.h2`
+	grid-area: temp;
+	font-size: 5rem;
+	text-align: center;
+	font-weight: 800;
+	color: ${({ theme }) => theme.textStrong};
+	text-shadow: ${({ theme }) => theme.textShadow};
+`;
+
+export const Icon = styled.div`
+	grid-area: icon;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	fill: ${({ theme }) => theme.icon};
+
+	svg {
+		filter: drop-shadow(${({ theme }) => theme.shadow});
+	}
+`;
+
+export const Details = styled.div`
+	grid-area: details;
 	display: flex;
 	flex-direction: column;
-	width: 50%;
-
+	justify-content: space-around;
 	p {
-		padding: 0.2rem 0;
-		font-size: 0.9rem;
-	}
-
-	h2 {
-		font-size: 3rem;
-		text-align: center;
-		font-weight: 800;
+		//padding: 0.2rem 0;
+		font-size: 1rem;
+		font-weight: 300;
+		//line-height: 1.5;
 	}
 `;
