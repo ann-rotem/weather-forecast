9e89689698500eb892e4950699397b4f65a5ff09
ann-rotem <ann.rotem@gmail.com>
2021-09-01 13:02:18 +0300

    refactor: add footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from "react";
 import * as S from "./Footer.style";
+import { GithubIcon } from "components/Icon";
 
 const Footer = () => {
 	return (
 		<S.Footer>
-			<p>Footer</p>
+			<S.Text>
+				Created by <span>Ann Rotem</span> 2021
+			</S.Text>
+			<S.Links>
+				<S.Link target="_blank" href="https://github.com/ann-rotem">
+					<GithubIcon size="lg" />
+				</S.Link>
+			</S.Links>
 		</S.Footer>
 	);
 };
