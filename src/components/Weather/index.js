f955edaba0474e84a8f00329d5f4ed6d73bf85d5
ann-rotem <ann.rotem@gmail.com>
2021-09-09 23:19:33 +0300

    refactor: add hourly component export

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,2 +1,3 @@
 export { default as CurrentWeather } from "./CurrentWeather/CurrentWeather";
 export { default as Forecast } from "./Forecast/Forecast";
+export { default as Hourly } from "./Hourly/Hourly";
