732a1f795b0e97dce6b59e670f3970f8aad39942
ann-rotem <ann.rotem@gmail.com>
2021-09-09 23:18:59 +0300

    refactor: remove unused component

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,6 @@ export { default as Button } from "./Button/Button";
 export { default as ErrorMessage } from "./ErrorMessage/ErrorMessage";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
-//export { default as Icon } from "./Icon/Icon";
-export { default as Main } from "./Main/Main";
 export { default as Search } from "./Search/Search";
 export { default as Tabs } from "./Tabs/Tabs";
 export { default as ThemeToggler } from "./ThemeToggler/ThemeToggler";
