f154e3a36f075f27180b4839aaa15ab836c03db6
ann-rotem <ann.rotem@gmail.com>
2021-09-09 23:22:54 +0300

    feat: add daily forecast component

--- src/components/Weather/Forecast/Forecast.js
+++ src/components/Weather/Forecast/Forecast.js
@@ -1,10 +1,30 @@
 import React from "react";
 import * as S from "./Forecast.style";
+import { WeatherIcon } from "components/Icon";
+import { round, convertUnixTime } from "utils/helpers/dateTime.helpers";
 
-const Forecast = () => {
+const Forecast = ({ data }) => {
 	return (
 		<S.Forecast>
-			<p>Forecast component</p>
+			<S.Days>
+				{data &&
+					data.map((day, index) => {
+						return (
+							<S.Day key={day.dt} isOdd={Boolean(index % 2)}>
+								<p>{convertUnixTime(day.dt, "date, weekday")}</p>
+
+								<p>
+									{round(day.temp.day)}&deg; - {round(day.temp.night)}
+									&deg;
+								</p>
+								<WeatherIcon
+									condition={day.weather[0].icon}
+									size="30"
+								/>
+							</S.Day>
+						);
+					})}
+			</S.Days>
 		</S.Forecast>
 	);
 };
--- src/components/Weather/Forecast/Forecast.style.js
+++ src/components/Weather/Forecast/Forecast.style.js
@@ -1,3 +1,48 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
+import { breakpoints } from "utils/constants/breakpoints";
 
-export const Forecast = styled.div``;
+export const Forecast = styled.div`
+	display: flex;
+	flex-direction: column;
+	max-height: calc(100% - 4.5rem);
+	border-radius: 0 0 1.5rem 1.5rem;
+	background-color: ${({ theme }) => theme.backgroundContrast};
+	overflow: hidden;
+`;
+
+export const Days = styled.div`
+	display: grid;
+	grid-template-rows: repeat(8, 3rem);
+	overflow-y: auto;
+	margin: 0 0.2rem 0.5rem 0;
+
+	/* @media only screen and (min-width: ${breakpoints.lg}px) {
+		grid-template-columns: repeat(8, auto);
+		grid-template-rows: 1fr;
+		margin: 0;
+	} */
+`;
+
+export const Day = styled.div`
+	display: flex;
+	width: 100%;
+	justify-content: space-around;
+	align-items: center;
+	background: ${(props) => !props.isOdd && props.theme.backgroundContrast};
+	color: ${(props) =>
+		props.isOdd ? props.theme.textStrong : props.theme.text};
+	padding: 0.3rem 1.5rem;
+
+	p {
+		min-width: 30%;
+	}
+
+	/* @media only screen and (min-width: ${breakpoints.lg}px) {
+		flex-direction: column;
+		overflow-x: auto;
+		padding: 0;
+		align-items: space-between;
+		justify-content: space-around;
+		height: 10rem;
+	} */
+`;
