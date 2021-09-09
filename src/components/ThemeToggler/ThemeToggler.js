45a8491f391f43641573e37c1062816181d1d957
ann-rotem <ann.rotem@gmail.com>
2021-09-09 23:08:32 +0300

    refactor: replace labelText prop with aria-label

--- src/components/ThemeToggler/ThemeToggler.js
+++ src/components/ThemeToggler/ThemeToggler.js
@@ -5,7 +5,7 @@ const ThemeToggler = (props) => {
 	const { theme, handleChange } = props;
 
 	return (
-		<S.Toggler onClick={handleChange} value={theme} labelText="theme toggle">
+		<S.Toggler onClick={handleChange} value={theme} aria-label="theme toggle">
 			{theme === "dark" ? <S.Stars></S.Stars> : <S.Clouds></S.Clouds>}
 			<S.Switch></S.Switch>
 		</S.Toggler>
