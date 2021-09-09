8534e80bc441d781c33dd2183dac04dab7b77943
ann-rotem <ann.rotem@gmail.com>
2021-09-09 23:01:18 +0300

    refactor: add hooks exports

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,3 +1,5 @@
 export { default as useBreakpoints } from "./useBreakpoints";
 export { default as useFetch } from "./useFetch";
 export { default as useGeolocation } from "./useGeolocation";
+export { default as useGetCoordsByName } from "./useGetCoordsByName";
+export { default as useGetWeatherByCoords } from "./useGetWeatherByCoords";
