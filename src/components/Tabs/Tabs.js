9035dbb4954d871b1881c3a97f85ae198620cb42
ann-rotem <ann.rotem@gmail.com>
2021-09-17 02:14:49 +0300

    refactor: add location and weather data props
    This commit adds the following props:
    - location data prop to display searched location information.
    - currentWeather, hourlyWeather, and dailyWeather props to
       display current weather and hourly and daily forecast data
       of the searched location.

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import * as S from "./Tabs.style";
-import { CurrentWeather, Forecast } from "components";
+import { CurrentWeather, Forecast, Hourly } from "components";
 import { getCurrentTime, getCurrentDate } from "utils/helpers/dateTime.helpers";
 import { LocationIcon } from "components/Icon";
 
 const types = ["now", "hourly", "daily"];
 
-const Tabs = ({ data }) => {
+const Tabs = ({ location, currentWeather, hourlyWeather, dailyWeather }) => {
 	const [active, setActive] = useState(types[0]);
 	const time = getCurrentTime();
 	const date = getCurrentDate();
@@ -26,20 +26,23 @@ const Tabs = ({ data }) => {
 
 			<S.TabContent>
 				<S.CurrentDetails>
-					<S.LocationWrapper>
-						<LocationIcon />
-						<p>
-							{data.name}, {data.sys.country}{" "}
-						</p>
-					</S.LocationWrapper>
-
+					{location && (
+						<S.LocationWrapper>
+							<LocationIcon />
+							<p>
+								{location.city}, {location.country}
+							</p>
+						</S.LocationWrapper>
+					)}
 					<S.CurrentDateTime>
 						{date} | {time}
 					</S.CurrentDateTime>
 				</S.CurrentDetails>
-				{active === "now" && <CurrentWeather data={data} />}
-				{active === "hourly" && <p>Hourly</p>}
-				{active === "daily" && <Forecast />}
+				<S.WeatherWrapper>
+					{active === "now" && <CurrentWeather data={currentWeather} />}
+					{active === "hourly" && <Hourly data={hourlyWeather} />}
+					{active === "daily" && <Forecast data={dailyWeather} />}
+				</S.WeatherWrapper>
 			</S.TabContent>
 		</S.Tabs>
 	);
