30554883392c85df27724b06a35fe7fca1066114
ann-rotem <ann.rotem@gmail.com>
2021-09-17 02:03:47 +0300

    fix: fix styling of Tabs component
    This commit prevents the unwanted overflowing effect that happens when
    The forecast tab is active and fixes color contrast.

--- src/components/Tabs/Tabs.style.js
+++ src/components/Tabs/Tabs.style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components/macro";
+import styled, { css } from "styled-components/macro";
 import { breakpoints } from "utils/constants/breakpoints";
 
 export const Tabs = styled.section`
@@ -6,66 +6,62 @@ export const Tabs = styled.section`
 	flex-direction: column;
 	margin: 0 auto;
 	width: 100%;
-	height: 100%;
-	overflow: hidden;
+	max-width: 25rem;
+	height: auto;
 	border-radius: 0 0 1.5rem 1.5rem;
+	margin-bottom: 1rem;
 
-	@media only screen and (min-width: ${breakpoints.sm}px) {
-		max-width: 25rem;
+	@media only screen and (min-width: ${breakpoints.lg}px) {
+		margin: 1rem auto;
 	}
-	/* @media only screen and (min-width: ${breakpoints.lg}px) {
-		max-height: 100%;
-		max-width: 40rem;
-	} */
 `;
 
 export const ButtonGroup = styled.div`
 	display: inline-flex;
 	margin: 0 auto;
-	height: 2rem;
-
-	@media only screen and (min-width: ${breakpoints.sm}px) {
-		height: 2.5rem;
-	}
 `;
 
 export const TabButton = styled.button`
 	display: inline-flex;
-	padding: 0.5rem 1rem 0.3rem 1rem;
-	margin: 0 0.3rem;
+	align-items: center;
+	padding: 0.3rem 1rem;
+	margin: 0 0.5rem;
 	font-size: 0.8rem;
 	font-weight: 300;
 	cursor: pointer;
-	color: ${(props) =>
-		props.active ? props.theme.textStrong : props.theme.text};
-	background: ${({ theme }) => theme.backgroundContrast};
+	color: ${({ theme }) => theme.text};
+	background: ${({ theme }) => theme.backgroundSecondary};
 	border-radius: 40% 40% 0 0;
 	border: none;
-	opacity: ${(props) => (props.active ? "1" : "0.4")};
-	box-shadow: ${({ theme }) => theme.shadow};
 	text-transform: capitalize;
+	box-shadow: -0.1rem 0rem 0.2rem rgba(40, 40, 40, 0.5);
+	${(props) =>
+		props.active &&
+		css`
+			color: ${({ theme }) => theme.textStrong};
+			background: ${({ theme }) => theme.backgroundPrimary};
+			box-shadow: -0.2rem 0rem 0.3rem rgba(50, 50, 50, 0.4);
+			cursor: default;
+		`}
 
 	&:hover {
 		color: ${(props) => props.theme.textStrong};
-		opacity: 0.8;
+		opacity: ${(props) => (props.active ? "1" : "0.7")};
 	}
 
 	@media only screen and (min-width: ${breakpoints.sm}px) {
-		padding: 0.7rem 1rem 0.4rem 1rem;
-		font-size: 1.2rem;
+		padding: 0.5rem 1rem 0.3rem 1rem;
+		font-size: 1rem;
 	}
 `;
 
 export const TabContent = styled.article`
 	display: flex;
 	flex-direction: column;
-	height: calc(100% - 2.5rem);
+	height: auto;
 	background-origin: border-box;
 	border-radius: 1.5rem;
-
-	@media only screen and (min-width: ${breakpoints.lg}px) {
-		//max-height: 80%;
-	}
+	box-shadow: ${({ theme }) => theme.shadow};
 `;
 
 export const CurrentDetails = styled.div`
@@ -73,13 +69,15 @@ export const CurrentDetails = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-	background-color: ${({ theme }) => theme.backgroundContrast};
+	background-color: ${({ theme }) => theme.backgroundPrimary};
 	border-radius: 1.5rem 1.5rem 0 0;
 	width: 100%;
-	height: 4.5rem;
+	height: 4rem;
 	padding: 0.2rem 0;
 	color: ${({ theme }) => theme.text};
-	border-bottom: 0.1rem solid ${({ theme }) => theme.backgroundPrimary};
+	fill: ${({ theme }) => theme.text};
+	border-bottom: 0.1rem solid ${({ theme }) => theme.backgroundSecondary};
+	z-index: 10;
 
 	@media only screen and (min-width: ${breakpoints.sm}px) {
 		padding: 0.5rem 0;
@@ -99,4 +97,16 @@ export const LocationWrapper = styled.span`
 		font-size: 1.2rem;
 		padding-left: 0.5rem;
 	}
+`;
+
+export const WeatherWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	height: 100%;
+	border-radius: 0 0 1.5rem 1.5rem;
+	background-color: ${({ theme }) => theme.backgroundContrast};
+	backdrop-filter: blur(0.5rem);
+	color: ${({ theme }) => theme.text};
+	height: auto;
 `;
