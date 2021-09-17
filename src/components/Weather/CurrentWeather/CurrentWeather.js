40e53f3172bf51d2605ea4179f9e788cf873cbed
ann-rotem <ann.rotem@gmail.com>
2021-09-17 03:14:40 +0300

    fix: update current weather data object deconstruction
    Previously, the current weather data prop contained openWeather
    current weather API call response. This call allows access to current
    weather data, whereas openWeather One Call API provides access
    to current weather, hourly forecast, and daily forecast for any
    geographical coordinates.
    
    This commit changes the current weather data object deconstruction
    to match the openWeather one call API data response.
    Current weather data collection: https://openweathermap.org/current

--- src/components/Weather/CurrentWeather/CurrentWeather.js
+++ src/components/Weather/CurrentWeather/CurrentWeather.js
@@ -1,24 +1,26 @@
 import React from "react";
 import * as S from "./CurrentWeather.style";
+import { Paragraph } from "../Weather.style";
 import { WeatherIcon } from "components/Icon";
 import { round, convertUnixTime } from "utils/helpers/dateTime.helpers";
 
 const CurrentWeather = ({ data }) => {
-	const sunrise = convertUnixTime(data.sys.sunrise, "time");
-	const sunset = convertUnixTime(data.sys.sunset, "time");
+	const sunrise = convertUnixTime(data.sunrise, "time");
+	const sunset = convertUnixTime(data.sunset, "time");
 	return (
 		<>
 			<S.CurrentWeather>
-				<S.Temp>{round(data.main.temp)}°</S.Temp>
+				<S.CurrentTemp shadow>
+					<h2>{round(data.temp)}&deg;</h2>
+					<Paragraph>Feels like {round(data.feels_like)}&deg;</Paragraph>
+				</S.CurrentTemp>
 				<S.Icon>
-					<WeatherIcon condition={data.weather[0].icon} size="120" />
+					<WeatherIcon condition={data.weather[0].icon} />
+					<Paragraph>{data.weather[0].description}</Paragraph>
 				</S.Icon>
 				<S.Details>
-					<p>Feels like {round(data.main.feels_like)}°</p>
-					<p>{data.weather[0].description}</p>
-					<p>Humidity: {data.main.humidity}%</p>
-					<p>Sunrise: {sunrise}</p>
-					<p>Sunset: {sunset}</p>
+					<Paragraph>Sunrise: {sunrise}</Paragraph>
+					<Paragraph>Sunset: {sunset}</Paragraph>
 				</S.Details>
 			</S.CurrentWeather>
 		</>
