54a256cfa92f8b89866cba60af156f87c8d9c522
ann-rotem <ann.rotem@gmail.com>
2021-09-17 11:02:16 +0300

    refactor: use Weather.style.js styled components
    This commit cleans up Forecast.style.js by merging
    together blocks from Weather.style.js

--- src/components/Weather/Forecast/Forecast.js
+++ src/components/Weather/Forecast/Forecast.js
@@ -1,31 +1,34 @@
 import React from "react";
 import * as S from "./Forecast.style";
+import { Paragraph, IconWrapper } from "../Weather.style";
 import { WeatherIcon } from "components/Icon";
 import { round, convertUnixTime } from "utils/helpers/dateTime.helpers";
 
 const Forecast = ({ data }) => {
 	return (
-		<S.Forecast>
-			<S.Days>
-				{data &&
-					data.map((day, index) => {
-						return (
-							<S.Day key={day.dt} isOdd={Boolean(index % 2)}>
-								<p>{convertUnixTime(day.dt, "date, weekday")}</p>
+		<S.Days>
+			{data &&
+				data.map((day, index) => {
+					return (
+						<S.Day key={day.dt} isOdd={Boolean(index % 2)}>
+							<Paragraph>
+								{convertUnixTime(day.dt, "date, weekday")}
+							</Paragraph>
 
-								<p>
-									{round(day.temp.day)}&deg; - {round(day.temp.night)}
-									&deg;
-								</p>
+							<Paragraph>
+								{round(day.temp.day)}&deg; - {round(day.temp.night)}
+								&deg;
+							</Paragraph>
+							<IconWrapper>
 								<WeatherIcon
 									condition={day.weather[0].icon}
 									size="30"
 								/>
-							</S.Day>
-						);
-					})}
-			</S.Days>
-		</S.Forecast>
+							</IconWrapper>
+						</S.Day>
+					);
+				})}
+		</S.Days>
 	);
 };
 
