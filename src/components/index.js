e7cc3e3f08abd48a5dba17eb95f3d1b12c9f0065
ann-rotem <ann.rotem@gmail.com>
2021-09-17 11:06:24 +0300

    fix: update default exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
-export { default as Button } from "./Button/Button";
 export { default as ErrorMessage } from "./ErrorMessage/ErrorMessage";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
+export { default as Info } from "./Info/Info";
+export { default as Logo } from "./Logo/Logo";
 export { default as Search } from "./Search/Search";
 export { default as Tabs } from "./Tabs/Tabs";
 export { default as ThemeToggler } from "./ThemeToggler/ThemeToggler";
