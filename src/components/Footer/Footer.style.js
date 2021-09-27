09b373d5d8973bc288e5b1b75154742aaf1f1447
ann-rotem <ann.rotem@gmail.com>
2021-09-27 11:04:59 +0300

    style: add uniform glass effect

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -1,19 +1,16 @@
 import styled from "styled-components";
 import { breakpoints } from "utils/constants/breakpoints";
+import { GlassEffect } from "GlobalStyle";
 
 export const Footer = styled.footer`
+	${GlassEffect}
 	grid-area: footer;
-	background: ${({ theme }) => theme.backgroundContrast};
-	backdrop-filter: blur(0.2rem);
-	color: ${({ theme }) => theme.textSecondary};
 	display: flex;
 	font-size: 0.9rem;
-	height: var(--footer-height);
 	width: 100%;
 	padding: 0 1rem;
 	align-items: center;
 	justify-content: space-between;
-	box-shadow: ${({ theme }) => theme.shadow};
 
 	@media only screen and (min-width: ${breakpoints.lg}px) {
 		display: none;
