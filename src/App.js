b05b541e6cac2e35b1b72ec745dccdf199e18b3f
ann-rotem <ann.rotem@gmail.com>
2021-09-27 11:07:58 +0300

    refactor: move main section to Main component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { GlobalStyle, lightTheme, darkTheme } from "GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import { ErrorMessage, Footer, Header, Search, Tabs } from "components";
-import { useGetCoordsByName, useGetWeatherByCoords } from "utils/hooks";
+import { Footer, Header, Main } from "components";
 
 function App() {
 	const [theme, setTheme] = useState("dark");
 	const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
 
-	const { coords, location, setQuery } = useGetCoordsByName();
-	const {
-		currentWeather,
-		hourlyWeather,
-		dailyWeather,
-		loading,
-		error,
-		setCoords,
-	} = useGetWeatherByCoords();
-
-	useEffect(() => {
-		if (!coords) return;
-		setCoords(coords);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [coords]);
-
-	const handleSearch = (searchTerm) => {
-		setQuery(searchTerm);
-	};
-
-	const getCurrentLocationCoords = (userCoords) => {
-		setCoords(userCoords);
-	};
-
 	return (
 		<ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
 			<GlobalStyle />
 			<Header theme={theme} toggleTheme={toggleTheme} />
-			<main>
-				<Search
-					handleSearch={handleSearch}
-					getCurrentLocationWeather={getCurrentLocationCoords}
-				/>
-				{loading ? (
-					<p>Loading...</p>
-				) : error ? (
-					<ErrorMessage error={error} />
-				) : currentWeather ? (
-					<Tabs
-						location={location}
-						currentWeather={currentWeather}
-						hourlyWeather={hourlyWeather}
-						dailyWeather={dailyWeather}
-					/>
-				) : null}
-			</main>
+			<Main />
 			<Footer />
 		</ThemeProvider>
 	);
