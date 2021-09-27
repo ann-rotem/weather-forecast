c055fd24970e9af7788f5e66ccb5c92096acf787
ann-rotem <ann.rotem@gmail.com>
2021-09-27 10:57:48 +0300

    feat: add and export new icons

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,5 +1,9 @@
 export { default as GithubIcon } from "./GithubIcon";
+export { default as HumidityIcon } from "./HumidityIcon";
 export { default as TargetIcon } from "./TargetIcon";
 export { default as LocationIcon } from "./LocationIcon";
 export { default as SearchIcon } from "./SearchIcon";
+export { default as SunriseIcon } from "./SunriseIcon";
+export { default as SunsetIcon } from "./SunsetIcon";
 export { default as WeatherIcon } from "./WeatherIcon";
+export { default as WindIcon } from "./WindIcon";
