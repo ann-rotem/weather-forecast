e0584a4bf321f23fb6f4e484cac92a096eb9486e
ann-rotem <ann.rotem@gmail.com>
2021-10-22 21:49:10 +0300

    refactor: remove logo image and unused style

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,10 @@
 import * as S from "./Logo.style";
-import LogoIcon from "./LogoIcon";
 
 const Logo = () => {
 	return (
 		<S.LogoWrapper>
-			<LogoIcon />
 			<S.LogoText>
-				<h1>
-					<span>Weather</span> Forecast
-				</h1>
+				<span>Weather</span> Forecast
 			</S.LogoText>
 		</S.LogoWrapper>
 	);
--- src/components/Logo/Logo.style.js
+++ src/components/Logo/Logo.style.js
@@ -1,47 +1,19 @@
 import styled from "styled-components/macro";
-import { breakpoints } from "utils/constants/breakpoints";
 
 export const LogoWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	max-height: 100%;
-
-	svg {
-		height: 100%;
-		max-height: 5rem;
-	}
-	@media only screen and (min-width: ${breakpoints.md}px) {
-		width: 17rem;
-	}
-
-	@media only screen and (min-width: ${breakpoints.lg}px) {
-		flex-direction: column;
-		padding-bottom: 2rem;
-		max-width: 100%;
-		height: auto;
-		row-gap: 1rem;
-		justify-content: center;
-	}
 `;
 
-export const LogoText = styled.div`
-	padding-left: 0.5rem;
-	width: auto;
-	display: inline-flex;
+export const LogoText = styled.h1`
+	font-weight: 800;
+	text-shadow: ${({ theme }) => theme.elevation.small};
+	color: ${({ theme }) => theme.colors.text.strong};
+	line-height: 1;
+	font-style: italic;
 
-	h1 {
-		font-size: 1.5em;
-		font-weight: 800;
-		text-shadow: -2px 1px 0 rgba(20, 20, 20, 0.2);
-		color: ${({ theme }) => theme.primary};
-	}
 	span {
 		font-weight: 500;
-	}
-
-	@media only screen and (min-width: ${breakpoints.lg}px) {
-		padding: 0.5rem 0;
-		text-align: center;
 	}
 `;
