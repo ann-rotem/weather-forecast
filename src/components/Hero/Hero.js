82670f89d19b2f67cf476f874fcdf12c766c9092
ann-rotem <ann.rotem@gmail.com>
2021-10-22 21:46:41 +0300

    refactor: minor refactoring/style changes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,24 @@
-import React from "react";
 import * as S from "./Hero.style";
+import HeroImage from "./HeroImage";
 
 const Hero = () => {
 	return (
 		<S.HeroWrapper>
-			<S.Heading>
-				Get weather forecast for your current location or search for a
-				different location
-			</S.Heading>
-			<S.Text>Please make sure you allow permission to geolocation</S.Text>
+			<S.Heading>Hourly, Daily and Current Weather Forecast</S.Heading>
+			<S.ImageWrapper>
+				<HeroImage />
+			</S.ImageWrapper>
+
+			{/* <S.Text weight="bold" size="1.5">
+				Anytime. Anywhere.
+			</S.Text> */}
+			<S.TextWrapper>
+				<S.Text>
+					*To provide you with more accurate local forecast,
+					<em>WeatherForecast</em> needs your permission to access the
+					device's location
+				</S.Text>
+			</S.TextWrapper>
 		</S.HeroWrapper>
 	);
 };
--- src/components/Hero/Hero.style.js
+++ src/components/Hero/Hero.style.js
@@ -1,28 +1,78 @@
 import styled from "styled-components/macro";
-//import { GlassEffect } from "GlobalStyle";
-import { breakpoints } from "utils/constants/breakpoints";
 
 export const HeroWrapper = styled.article`
-	display: flex;
-	flex-direction: column;
-	text-align: center;
-	max-width: 30rem;
-	margin: 0 1rem;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: auto 1fr auto;
+	grid-template-areas:
+		"heading"
+		"image"
+		"text";
+	gap: 1rem;
+	align-items: center;
+	width: 100%;
+	max-width: 20rem;
+	margin: 1rem;
+	padding: 1rem;
 	border-radius: 1rem;
+	background: ${({ theme }) => theme.colors.background.primary};
+	box-shadow: ${({ theme }) => theme.elevation.small};
+
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.tablet}) {
+		padding: 1rem 2rem;
+		margin: auto 1rem;
+		grid-template-columns: 1fr 15rem;
+		grid-template-rows: auto 1fr;
+		grid-template-areas:
+			"heading heading"
+			"text image"
+			"text image";
+		gap: 0;
+		max-width: 30rem;
+	} ;
 `;
 
 export const Heading = styled.h2`
-	color: ${({ theme }) => theme.textStrong};
-	font-size: 1.8em;
-	margin-bottom: 1em;
-	text-shadow: ${({ theme }) => theme.shadow};
+	grid-area: heading;
+	color: ${({ theme }) => theme.colors.text.strong};
+	font-size: 1.8rem;
+	margin-bottom: 0.5em;
+	text-align: center;
+	line-height: 1.3;
+`;
 
-	@media only screen and (min-width: ${breakpoints.md}px) {
-		font-size: 2.2em;
+export const ImageWrapper = styled.div`
+	grid-area: image;
+	display: flex;
+	justify-self: center;
+	width: 100%;
+	max-width: 20rem;
+	padding: 0 1rem;
+	svg {
+		width: 100%;
 	}
 `;
 
+export const TextWrapper = styled.div`
+	grid-area: text;
+	display: flex;
+	height: 100%;
+	align-items: end;
+`;
+
 export const Text = styled.p`
-	font-family: "Assistant", sans-serif;
 	font-size: 0.8em;
+	letter-spacing: 1px;
+	text-align: center;
+
+	em {
+		color: ${({ theme }) => theme.colors.text.strong};
+		padding: 0 0.2em;
+	}
+
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.tablet}) {
+		text-align: start;
+	} ;
 `;
