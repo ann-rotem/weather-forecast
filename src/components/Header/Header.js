5f76957fd6768f4923edc325eebc2f3f7af5ae4a
ann-rotem <ann.rotem@gmail.com>
2021-10-22 21:43:50 +0300

    refactor: remove unnecessary elements from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,14 @@
 import * as S from "./Header.style";
 import Logo from "components/Logo/Logo";
 import { ThemeToggler } from "components";
-import Info from "components/Info/Info";
 
 const Header = (props) => {
 	const { theme, toggleTheme } = props;
 
 	return (
 		<S.Header>
-			<S.HeaderMain>
-				<Logo />
-				<ThemeToggler theme={theme} handleChange={toggleTheme} />
-			</S.HeaderMain>
-			<S.HeaderBottom>
-				<Info />
-			</S.HeaderBottom>
+			<Logo />
+			<ThemeToggler theme={theme} handleChange={toggleTheme} />
 		</S.Header>
 	);
 };
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,41 +1,17 @@
 import styled from "styled-components/macro";
-import { breakpoints } from "utils/constants/breakpoints";
-import { GlassEffect } from "GlobalStyle";
 
 export const Header = styled.header`
-	${GlassEffect}
-	grid-area: header;
 	display: flex;
-	padding: 0 1rem;
-
-	@media only screen and (min-width: ${breakpoints.sm}px) {
-		padding: 0.5rem 2rem;
-	}
-
-	@media only screen and (min-width: ${breakpoints.lg}px) {
-		flex-direction: column;
-		justify-content: space-between;
-		padding: 2rem 0.5rem;
-	}
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	height: 3.5rem;
+	padding: 0 var(--horizontal-padding);
 `;
+
 export const HeaderMain = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
 	width: 100%;
-
-	@media only screen and (min-width: ${breakpoints.lg}px) {
-		flex-direction: column;
-	}
-`;
-
-export const HeaderBottom = styled.div`
-	display: none;
-
-	@media only screen and (min-width: ${breakpoints.lg}px) {
-		display: flex;
-		flex-direction: column-reverse;
-		justify-content: center;
-		align-items: center;
-	}
 `;
