51fe7ed1a0a7190a59c604d04bd800d80aec4767
ann-rotem <ann.rotem@gmail.com>
2021-11-09 15:50:36 +0200

    refactor: add title, details and prop types

--- src/components/Weather/CurrentWeather/CurrentWeather.js
+++ src/components/Weather/CurrentWeather/CurrentWeather.js
@@ -1,30 +1,80 @@
-import React from "react";
 import * as S from "./CurrentWeather.style";
-import { Paragraph } from "../Weather.style";
-import { WeatherIcon } from "components/Icon";
-import { round, convertUnixTime } from "utils/helpers/dateTime.helpers";
+import PropTypes from "prop-types";
+import { Text, Title } from "../Weather.style";
+import { WeatherIcon, Icon } from "components/Icon";
 
 const CurrentWeather = ({ data }) => {
-	const sunrise = convertUnixTime(data.sunrise, "time");
-	const sunset = convertUnixTime(data.sunset, "time");
 	return (
-		<>
-			<S.CurrentWeather>
+		<S.CurrentWeather>
+			<Title>Current Weather</Title>
+			<S.Wrapper>
 				<S.CurrentTemp shadow>
-					<h2>{round(data.temp)}&deg;</h2>
-					<Paragraph>Feels like {round(data.feels_like)}&deg;</Paragraph>
+					<h2>{data.temp}&deg;</h2>
+					<p>Feels like {data.feels_like}&deg;</p>
 				</S.CurrentTemp>
-				<S.Icon>
-					<WeatherIcon condition={data.weather[0].icon} />
-					<Paragraph>{data.weather[0].description}</Paragraph>
+				<S.Description>{data.description}</S.Description>
+				<S.Icon size="8">
+					<WeatherIcon condition={data.icon} />
 				</S.Icon>
-				<S.Details>
-					<Paragraph>Sunrise: {sunrise}</Paragraph>
-					<Paragraph>Sunset: {sunset}</Paragraph>
-				</S.Details>
-			</S.CurrentWeather>
-		</>
+			</S.Wrapper>
+
+			<S.Details>
+				<S.Detail>
+					<S.DetailIcon>
+						<Icon name="humidity" />
+						<p>Humidity</p>
+					</S.DetailIcon>
+					<Text color="strong" size="1.1">
+						{data.humidity}%
+					</Text>
+				</S.Detail>
+				<S.Detail>
+					<S.DetailIcon>
+						<Icon name="wind" />
+						<p>Wind</p>
+					</S.DetailIcon>
+					<Text color="strong" size="1.1">
+						{data.wind} m/s
+					</Text>
+				</S.Detail>
+				<S.Detail>
+					<S.DetailIcon>
+						<Icon name="sunrise" />
+						<p>UVI</p>
+					</S.DetailIcon>
+					<Text color="strong" size="1.1">
+						{data.uvi}
+					</Text>
+				</S.Detail>
+				<S.Detail>
+					<S.DetailIcon>
+						<Icon name="sunset" />
+						<p>Dew Point</p>
+					</S.DetailIcon>
+					<Text color="strong" size="1.1">
+						{data.dew}&deg;
+					</Text>
+				</S.Detail>
+			</S.Details>
+		</S.CurrentWeather>
 	);
+};
+
+CurrentWeather.propTypes = {
+	data: PropTypes.shape({
+		temp: PropTypes.number,
+		feels_like: PropTypes.number,
+		icon: PropTypes.string,
+		description: PropTypes.string,
+		humidity: PropTypes.number,
+		precipitation: PropTypes.number,
+		clouds: PropTypes.number,
+		wind: PropTypes.number,
+		uvi: PropTypes.number,
+		dew: PropTypes.number,
+		sunset: PropTypes.string,
+		sunrise: PropTypes.string,
+	}),
 };
 
 export default CurrentWeather;
--- src/components/Weather/CurrentWeather/CurrentWeather.style.js
+++ src/components/Weather/CurrentWeather/CurrentWeather.style.js
@@ -1,45 +1,171 @@
-import styled from "styled-components";
-import { breakpoints } from "utils/constants/breakpoints";
-import { Temperature, IconWrapper } from "../Weather.style";
+import styled from "styled-components/macro";
+import { WeatherCard, ContentWrapper, IconWrapper } from "../Weather.style";
 
-export const CurrentWeather = styled.div`
+export const CurrentWeather = styled(WeatherCard)`
+	grid-area: current;
 	display: grid;
-	grid-template-columns: auto 1fr;
-	grid-template-columns: auto fit-content(13rem);
-	grid-template-rows: repeat(auto-fit, minmax(4rem, 1fr));
-	gap: 0 1rem;
+	grid-template-columns: 1fr;
+	grid-template-rows: repeat(3, auto);
+	grid-template-areas:
+		"title"
+		"wrapper1"
+		"wrapper2";
+
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.laptop}) {
+		//align-self: center;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: min-content 1fr;
+		grid-template-areas:
+			"title ."
+			"wrapper1 wrapper2";
+		gap: 0 1rem;
+		max-width: 35rem;
+	}
+`;
+
+export const Wrapper = styled(ContentWrapper)`
+	grid-area: wrapper1;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: repeat(2, min-content);
 	grid-template-areas:
 		"temp icon"
-		"details icon";
-	padding: 1rem;
+		"description icon";
+	width: 100%;
+	padding: ${({ theme }) => theme.spacing.small};
+	margin-bottom: ${({ theme }) => theme.spacing.medium};
+
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.laptop}) {
+		padding: ${({ theme }) => theme.spacing.medium};
+		margin-bottom: 0;
+		gap: 0 ${({ theme }) => theme.spacing.small};
+	}
 `;
 
-export const CurrentTemp = styled(Temperature)`
-	grid-area: temp;
-	font-size: 1.5em;
-`;
+// export const Date = styled(Text)`
+// 	grid-area: date;
+// 	text-align: center;
+// 	padding-bottom: ${({ theme }) => theme.spacing.small};
+// `;
 
 export const Icon = styled(IconWrapper)`
 	grid-area: icon;
-	flex-direction: column;
-	align-items: center;
-	height: auto;
+	justify-self: center;
 
 	svg {
-		max-width: 100%;
+		max-height: 25em;
+		filter: drop-shadow(${({ theme }) => theme.icon.elevation.medium});
 	}
 
-	@media only screen and (min-width: ${breakpoints.lg}px) {
-		align-items: flex-end;
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.laptop}) {
+		height: 7em;
 	}
 `;
 
-export const Details = styled.div`
-	grid-area: details;
+export const CurrentTemp = styled.div`
+	grid-area: temp;
+	padding-left: 0.5em;
+	line-height: 1;
+
+	h2 {
+		font-size: 4rem;
+		color: ${({ theme }) => theme.colors.text.strong};
+		font-weight: 800;
+	}
+
+	p {
+		font-size: 1em;
+		font-weight: light;
+		text-transform: capitalize;
+	}
+`;
+
+export const Description = styled.p`
+	grid-area: description;
+	align-self: end;
+	padding-left: 0.5em;
+	word-wrap: normal;
+
+	text-transform: capitalize;
+`;
+
+export const Details = styled(ContentWrapper)`
+	grid-area: wrapper2;
+	display: flex;
+	width: 100%;
+	height: 100%;
+	justify-content: space-between;
+
+	> * {
+		&:not(:first-child) {
+			border-left: 1px solid ${({ theme }) => theme.colors.border.secondary};
+		}
+	}
+
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.laptop}) {
+		flex-direction: column;
+		justify-content: space-around;
+		padding: 0 ${({ theme }) => theme.spacing.small};
+
+		> * {
+			&:not(:first-child) {
+				border-left: none;
+				border-top: 1px solid ${({ theme }) => theme.colors.text.weak};
+			}
+		}
+	}
+`;
+
+export const Detail = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: flex-end;
-	font-size: 1em;
-	font-weight: 300;
-	line-height: 2;
+	width: 25%;
+	line-height: 1.2;
+	height: 100%;
+	align-items: center;
+	justify-content: space-between;
+	text-align: center;
+	font-weight: ${({ theme }) => theme.font.weight.light};
+	padding: ${({ theme }) => theme.spacing.small};
+
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.laptop}) {
+		flex-direction: row;
+		width: 100%;
+		height: 25%;
+		padding: 0;
+	}
+`;
+
+export const DetailIcon = styled.span`
+	display: inline-flex;
+	flex-direction: column;
+	height: 100%;
+
+	svg {
+		height: 100%;
+		max-height: 3rem;
+		width: auto;
+		padding: ${({ theme }) => theme.spacing.small};
+	}
+
+	@media only screen and (min-width: ${({ theme }) =>
+			theme.breakpoints.laptop}) {
+		flex-direction: row;
+		align-items: center;
+		svg {
+			max-height: 2rem;
+			padding: ${({ theme }) => theme.spacing.xSmall};
+		}
+
+		p {
+			font-size: 0.9em;
+			padding-left: 0.3em;
+			margin: auto 0;
+		}
+	}
 `;
